e18a890306eb912e96cf1613c58f64cf8ffe712b
Christopher Czach <chrisczach@gmail.com>
2018-12-07 23:32:17 -0800

    add test and dev environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+console.log('ENV*****', process.env.MONGODB_URI);
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -8,7 +18,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
